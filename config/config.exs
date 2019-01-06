055ea964a45b43269034fad0b6f81c7d358026e3
Coby Benveniste <coby.b@spot.im>
2019-01-06 13:15:59 +0200

    gitignore

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,2 @@
 use Mix.Config
-
-config :redix_cluster,
-  cluster_nodes: [
-    %{host: "staging.lygt2n.clustercfg.use1.cache.amazonaws.com", port: 6379}
-  ],
-  pool_size: 5,
-  pool_max_overflow: 0
+import_config "#{Mix.env()}.exs"
